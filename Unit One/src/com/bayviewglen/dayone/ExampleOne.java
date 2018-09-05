520049f3bb5873e5388e9b39e20fded4d6035458
jslightham <31053827+jslightham@users.noreply.github.com>
2018-09-05 17:44:15 -0400

    Initial

---
+++ Unit One/src/com/bayviewglen/dayone/ExampleOne.java
@@ -0,0 +1,22 @@
+package com.bayviewglen.dayone;
+
+public class ExampleOne {
+
+	public static void main(String[] args) {
+		// This is a comment
+		// Comments are meant for developers/programmers to describe what code
+		//does.
+		
+		/* 
+		 *This comment spans multiple lines
+		 */
+		
+		System.out.println("This is going to the console.1"); 
+		System.out.print("This is \"going\" to the console.2"); 
+		System.out.println("This is going to the console.3"); 
+		System.out.print("This is going to the console.3");
+		//escape sequence escapes the regular purpose of a character
+		// \\ slash, \n enter, \" quote, \' single quote, \t is tab
+	}
+	
+}
---
+++ Unit One/src/com/bayviewglen/dayone/HomeworkOne.java
@@ -0,0 +1,11 @@
+package com.bayviewglen.dayone;
+
+public class HomeworkOne {
+
+	public static void main(String[] args) {
+		System.out.println("JJJJJJJJJJJJJJ \t \t SSSSSSSSS");
+		System.out.println("       J                 S");
+		System.out.println("       J                 S");
+	}
+	
+}
