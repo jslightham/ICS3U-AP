ca3e270e8dc39b7a7aa0750145baad46a50f46d4
jslightham <31053827+jslightham@users.noreply.github.com>
2018-09-06 17:15:54 -0400

    Homework 1

--- Unit One/src/com/bayviewglen/dayone/HomeworkOne.java
+++ Unit One/src/com/bayviewglen/dayone/HomeworkOne.java
@@ -3,9 +3,52 @@ package com.bayviewglen.dayone;
 public class HomeworkOne {
 
 	public static void main(String[] args) {
+		questionOne();
+		System.out.println("\n");
+		questionTwo();
+		System.out.println("\n");
+		questionThree();
+		System.out.println("\n");
+		questionFour();
+	}
+	
+	//First Question
+	//Print Initials
+	public static void questionOne() {
 		System.out.println("JJJJJJJJJJJJJJ \t \t SSSSSSSSS");
 		System.out.println("       J                 S");
 		System.out.println("       J                 S");
+		System.out.println("       J \t \t SSSSSSSSS");
+		System.out.println("       J \t \t         S");
+		System.out.println("     JJ  \t \t         S");
+		System.out.println("    JJ    \t \t         S");
+		System.out.println(" JJJ     \t \t SSSSSSSSS");
+	}
+	//Second Question
+	//Print Image
+	public static void questionTwo() {
+		System.out.println(" o");
+		System.out.println("-|-");
+		System.out.println("/\\");
+	}
+	//Third Question
+	//Print Mailing Address (Randomly Generated Address)
+	public static void questionThree() {
+		System.out.println("   384 Warren Dr. ");
+		System.out.println("Yarmouth, NS B5A T9V");
+	}
+	//Question Four
+	//Print 
+	public static void questionFour() {
+		System.out.println("/////////////////////////////////////////////////////////////////////");
+		System.out.println("/ \t \t \t \t \t \t \t \t    /");
+		System.out.println("/ \t \t \t Our Lady of Mercy Hospital \t \t    /");
+		System.out.println("/ \t \t   ------------------------------------- \t    /");
+		System.out.println("/ \t \t \t \t \t \t \t \t    /");
+		System.out.println("/ \t \t             1306 Central City \t \t  \t    /");
+		System.out.println("/ \t \t \t    Phone (416) 555-1212 \t \t    /");
+		System.out.println("/ \t \t \t \t \t \t \t \t    /");
+		System.out.println("/////////////////////////////////////////////////////////////////////");
 	}
 	
 }
