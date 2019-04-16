d3312f02eb34991773f952b3e78127379abd09d1
jslightham <31053827+jslightham@users.noreply.github.com>
2019-04-16 11:10:32 -0400

    review

---
+++ Review/src/com/bayviewglen/dayone/NumberSystem.java
@@ -0,0 +1,29 @@
+package com.bayviewglen.dayone;
+
+public class NumberSystem {
+	public static void main(String[] args) {
+		/*
+		int maxInt = Integer.MAX_VALUE;
+		int minInt = Integer.MIN_VALUE;
+		
+		System.out.println(maxInt);
+		System.out.println(minInt);
+		
+		byte maxByte = Byte.MAX_VALUE;
+		byte minByte = Byte.MIN_VALUE;
+		
+		System.out.println(maxByte);
+		System.out.println(minByte);
+		
+		short maxShort = Short.MAX_VALUE;
+		short minShort = Short.MIN_VALUE;
+		
+		System.out.println(maxShort);
+		System.out.println(minShort);
+		*/
+		System.out.println((int)0b1011101);
+		System.out.println((int)0x7A3F);
+		
+		
+	}
+}
